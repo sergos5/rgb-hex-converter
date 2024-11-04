41d4cdbf9557882e57be899c16edfa36ef551931
Sergey Opekunov <sergos5@yandex.ru>
2024-11-04 18:01:56 +0300

    add: script

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
+const rgbInput = document.querySelectorAll('.rgb-block input')
+const convertBtn = document.querySelector('.convert-btn')
+const hexData = document.querySelector('.hex-data')
+const colorBlock = document.querySelector('.color-block')
 
-
+const getRGBColor = () => {
+    let rgbColorArr = []
+    rgbInput.forEach(item => {
+        rgbColorArr.push(+item.value)
+    })
+    
+    convertRGBToHex(rgbColorArr);    
+}
 
 const convertRGBToHex = (arrRGB)=> {
     let hex ='#'
     arrRGB.forEach(element => {     
         hex+=element.toString(16);            
     }); 
-    console.log(hex.toUpperCase());  
+    hexData.textContent=hex.toUpperCase();  
+    colorBlock.style.backgroundColor=hex
 }
 
-convertRGBToHex([44, 183, 66])
+convertBtn.addEventListener('click', ()=> {
+    getRGBColor()    
+})
+
+
+
 
