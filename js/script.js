738b91d56c123a105603e5322a4892c687898db0
Sergey Opekunov <sergos5@yandex.ru>
2024-11-04 11:15:43 +0300

    add: RGB-block

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('hello, world');
+
+
+
+const convertRGBToHex = (arrRGB)=> {
+    let hex ='#'
+    arrRGB.forEach(element => {     
+        hex+=element.toString(16);            
+    }); 
+    console.log(hex.toUpperCase());  
+}
+
+convertRGBToHex([44, 183, 66])
+
