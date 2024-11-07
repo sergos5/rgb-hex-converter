0b755dd1790c66853dd16e1013581b8082ad6deb
sergey fb <ooofb@mail.ru>
2024-11-07 17:12:49 +0300

    добавил проверку input

--- js/script.js
+++ js/script.js
@@ -2,12 +2,24 @@ const rgbInput = document.querySelectorAll('.rgb-block input')
 const convertBtn = document.querySelector('.convert-btn')
 const hexData = document.querySelector('.hex-data')
 const colorBlock = document.querySelector('.color-block')
+const errMessage = document.querySelector('.error-mess')
 
 convertBtn.disabled = 'true'
 
 rgbInput.forEach(item => {
     item.addEventListener('input', () => {
-
+        let val = item.value.replace(/\D/gi, '')
+        val = val.substring(0, 3)
+        if (val > 255) {
+            errMessage.classList.add('act')
+            item.classList.add('error-input')
+            console.log('макс. значение 255!!!');
+        } else {
+            errMessage.classList.remove('act')
+            item.classList.remove('error-input')
+        }
+        item.value = val
+        console.log(item.value);
     })
 })
 
